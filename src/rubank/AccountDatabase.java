f1a61bf1ce9a63a9ddb53971bd78d16df84ed75e
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-11 13:01:54 -0400

    Add context names

--- src/rubank/AccountDatabase.java
+++ src/rubank/AccountDatabase.java
@@ -1,54 +1,77 @@
 package rubank;
 
 public class AccountDatabase {
-    private Account [] accounts; //list of various types of accounts
+    private Account[] accounts; //list of various types of accounts
     private int numberOfAccounts; //number of accounts in the array
 
     /**
-     *
      * @param account
      * @return account number
      */
     private int find(Account account) {
         //search for an account in the array
-        for (int accountNumber = 0; accountNumber < numberOfAccounts; accountNumber++){
-            if(accounts[accountNumber].equals(account)){
+        for (int accountNumber = 0; accountNumber < numberOfAccounts; accountNumber++) {
+            if (accounts[accountNumber].equals(account)) {
                 return accountNumber;
             }
         }
         return 0;
     }
 
-    private void grow(){
+    private void grow() {
         //increase the capacity by 4
         Account[] newAccountDatabase = new Account[numberOfAccounts + 4];
-        for(int accountNumber = 0;  accountNumber < accounts.length; accountNumber++){
+        for (int accountNumber = 0; accountNumber < accounts.length; accountNumber++) {
             newA[accountNumber] = accounts[accountNumber];
         }
         accounts = newAccountDatabase;
-        }
+    }
+
     public boolean contains(Account account) {
         //overload if necessary
     }
+
     public boolean open(Account account) {
         //add a new account
     }
+
     public boolean close(Account account) {
         //remove the given account
     }
+
     public boolean withdraw(Account account) {
         //false if insufficient fund
         return account.balance > 0;
     }
+
     public void deposit(Account account) {
 
     }
+
+    /**
+     * Prints sorted by account type and profile.
+     *
+     * @author Ayaan Qayyum
+     */
     public void printSorted() {
-        //sort by account type and profile
+
     }
+
+    /**
+     * Prints account interest and fees.
+     *
+     * @author Ayaan Qayyum
+     */
     public void printFeesAndInterests() {
-        //calculate interests/fees
+
     }
+
+    /**
+     * Prints account updated balance after applying interest and fees.
+     *
+     * @author Ayaan Qayyum
+     */
     public void printUpdatedBalances() {
-        //apply the interests/fees
+
     }
+}
