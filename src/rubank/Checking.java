c83904a89c0945575d323e189ba029d279dc8544
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-11 12:54:40 -0400

    Fix checking monthly interest

--- src/rubank/Checking.java
+++ src/rubank/Checking.java
@@ -3,10 +3,17 @@ package rubank;
 public class Checking extends Account {
     private final int MONTHLY_FEE = 12;
     private final double ANNUAL_INTEREST = 1.0;
+
+    /**
+     * Returns annual interest.
+     * @return ANNUAL_INTEREST
+     * @author Ayaan Qayyum
+     */
     @Override
     public double monthlyInterest() {
-        return 0;
+        return ANNUAL_INTEREST;
     }
+
     /**
      * Checks if account balance is >= 1000, else applies fee.
      * @return MONTHLY_FEE
