b647b5fd9a4429a918740b272f36cba3585d6eb1
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-11 12:33:45 -0400

    Add monthly fee and interest in Checking

--- src/rubank/Checking.java
+++ src/rubank/Checking.java
@@ -1,8 +1,25 @@
 package rubank;
 
-public class Checking extends Savings {
-    public static final double checkingInterest = 1.0;
+public class Checking extends Account {
+    private final int MONTHLY_FEE = 12;
+    private final double ANNUAL_INTEREST = 1.0;
+    @Override
+    public double monthlyInterest() {
+        return 0;
+    }
+    /**
+     * Checks if account balance is >= 1000, else applies fee.
+     * @return MONTHLY_FEE
+     * @author Ayaan Qayyum
+     */
+    @Override
+    public double monthlyFee() {
+        if (balance >= 1000) return 0;
+        else return MONTHLY_FEE;
+    }
 
-
-
+    @Override
+    public int compareTo(Account o) {
+        return 0;
+    }
 }
