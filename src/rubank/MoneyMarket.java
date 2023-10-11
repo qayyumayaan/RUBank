1cf0aff6415ae393c9becc04636c7643137ee155
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-11 12:50:17 -0400

    Implement monthlyFee and Interest method in MoneyMarket

--- src/rubank/MoneyMarket.java
+++ src/rubank/MoneyMarket.java
@@ -1,6 +1,24 @@
 package rubank;
 
 public class MoneyMarket extends Account{
-    private int withdrawal;
-    public static final double moneyMarketInterest = 4.5;
+    private int withdrawal; // number of withdrawals
+    public static final double ANNUAL_INTEREST = 4.75;
+    public static final double MONTHLY_FEE = 25.0;
+
+    @Override
+    public double monthlyInterest() {
+        return moneyMarketAnnualInterest;
+    }
+
+    @Override
+    public double monthlyFee() {
+        if (balance >= 2000) return 0;
+        else return moneyMarketMonthlyFee;
+    }
+
+    @Override
+    public int compareTo(Account o) {
+        return 0;
+    }
+
 }
