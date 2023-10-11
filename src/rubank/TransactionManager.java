900d4c7a30264a2e2ca8b40ef3bca97e8a794902
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-10 20:08:14 -0400

    Add cases in Transaction Manager

--- src/rubank/TransactionManager.java
+++ src/rubank/TransactionManager.java
@@ -1,4 +1,51 @@
 package rubank;
 
+import java.util.Objects;
+
 public class TransactionManager {
+    /**
+     * Runs the program, handles input and commands O, C, D, W, P, PI, UB, Q
+     * @author Ayaan Qayyum
+     */
+    public void run() {
+        System.out.println("Transaction Manager running...");
+        while (!Objects.equals(command, "Q")) {
+            String input = scanner.nextLine();
+            parts = input.split("\\s+");
+            if (parts.length == 0) {
+                System.out.println("Invalid input!");
+                continue;
+            }
+            command = parts[0];
+            switch (command) {
+                case ("O"):
+                    caseOpen();
+                    break;
+                case ("C"):
+                    caseClose();
+                    break;
+                case ("D"):
+                    caseDeposit();
+                    break;
+                case ("W"):
+                    caseWithdraw();
+                    break;
+                case ("P"):
+                    caseDisplay();
+                    break;
+                case ("PI"):
+                    caseDisplayWithDetails();
+                    break;
+                case ("UB"):
+                    caseUpdateBalance();
+                    break;
+                case ("Q"):
+                    System.out.println("Transaction Manager is terminated.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println(command + " is an invalid command!");
+            }
+        }
+    }
 }
