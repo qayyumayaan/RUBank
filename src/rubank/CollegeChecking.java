00fe2ee94ca26f0dc3ed4248d1eef75de1066bcd
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-11 12:54:53 -0400

    Implement monthlyFee and Interest in CollegeChecking

--- src/rubank/CollegeChecking.java
+++ src/rubank/CollegeChecking.java
@@ -1,6 +1,23 @@
 package rubank;
 
 public class CollegeChecking extends Checking {
+    private final double MONTHLY_FEE = 0;
+
+    private final double ANNUAL_INTEREST = 1.0;
+
     private Campus campus;
-    public static final double checkingInterest = 1.0;
+
+    /**
+     * Checks if account balance is >= 1000, else applies fee.
+     * @return MONTHLY_FEE
+     * @author Ayaan Qayyum
+     */
+    @Override
+    public double monthlyFee() {
+        return MONTHLY_FEE;
+    }
+
+    public double monthlyInterest() {
+        return ANNUAL_INTEREST;
+    }
 }
