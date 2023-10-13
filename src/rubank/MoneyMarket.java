0f05902317d4653817d95a32ae68df19f600c85a
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-13 09:36:09 -0400

    Add loyalty checks and withdraw penalty in MoneyMarket

--- src/rubank/MoneyMarket.java
+++ src/rubank/MoneyMarket.java
@@ -3,7 +3,12 @@ package rubank;
 public class MoneyMarket extends Account{
     private int withdrawal; // number of withdrawals
     public static final double ANNUAL_INTEREST = 4.75;
+    public static final double LOYAL_CUSTOMER_ADDITION = 0.25; // Loyal customer addition
+
     public static final double MONTHLY_FEE = 25.0;
+    public static final double WITHDRAWAL_PENALTY = 10.0;
+
+    protected boolean isLoyal;
 
     /**
      * Initializes MoneyMarket.
@@ -14,27 +19,37 @@ public class MoneyMarket extends Account{
         this.holder = profile;
         this.balance = quantity;
         this.withdrawal = 0;
+        updateLoyaltyStatus();
     }
 
     /**
-     * Returns the monthly interest.
+     * Returns the monthly interest, accounting for loyal customers.
      * @author Ayaan Qayyum
      * @return monthly interest
      */
     @Override
     public double monthlyInterest() {
-        return ANNUAL_INTEREST / 12;
+        double interestRate = ANNUAL_INTEREST;
+        if (isLoyal) {
+            interestRate += LOYAL_CUSTOMER_ADDITION;
+        }
+        return interestRate / 12;
     }
 
     /**
-     * Returns the monthly interest.
+     * Returns the monthly fee.
      * @author Ayaan Qayyum
      * @return monthly fee.
      */
     @Override
     public double monthlyFee() {
-        if (balance >= 2000) return 0;
-        else return MONTHLY_FEE;
+        if (balance >= 2000) {
+            return 0;
+        } else if (withdrawal > 3) {
+            return WITHDRAWAL_PENALTY + MONTHLY_FEE;
+        } else {
+            return MONTHLY_FEE;
+        }
     }
 
     /**
@@ -43,12 +58,27 @@ public class MoneyMarket extends Account{
      * @return new number of withdrawals.
      */
     public int updateNumberOfWithdrawals() {
-        return withdrawal++;
+        withdrawal++;
+        updateLoyaltyStatus();
+        return withdrawal;
     }
+
 
     @Override
     public int compareTo(Account other) {
         return super.compareTo(other);
+    }
+
+    /**
+     * Updates the loyalty status based on balance.
+     * @author Ayaan Qayyum
+     */
+    private void updateLoyaltyStatus() {
+        if (this.balance >= 2000) {
+            this.isLoyal = true;
+        } else {
+            this.isLoyal = false;
+        }
     }
 
 }
